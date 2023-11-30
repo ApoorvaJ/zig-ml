6841c1204e8dbac53032c77b4362b3809082d093
Apoorva Joshi <apoorvaj@apoorvaj.io>
2023-11-30 19:21:22 +0100

    Some arg parsing and binary loading logic

--- src/main.zig
+++ src/main.zig
@@ -1,17 +1,65 @@
 const std = @import("std");
 
+const Config = struct {
+    dim: i32, // transformer dimension
+    hidden_dim: i32, // for ffn layers
+    n_layers: i32, // number of layers
+    n_heads: i32, // number of query heads
+    n_kv_heads: i32, // number of key/value heads (can be < query heads because of multiquery)
+    vocab_size: i32, // vocabulary size, usually 256 (byte-level)
+    seq_len: i32, // max sequence length
+};
+
+const Transformer = struct {
+    config: Config,
+};
+
+fn transformerInit(checkpoint_path: []const u8) !Transformer {
+    const file = try std.fs.cwd().openFile(checkpoint_path, .{});
+    defer file.close();
+
+    var config: Config = undefined;
+    const config_slice = std.mem.asBytes(&config);
+    _ = try file.read(config_slice);
+
+    return .{ .config = config };
+}
+
+fn errorUsage() !void {
+    const stderr = std.io.getStdErr().writer();
+    const usage =
+        \\Usage:   run <checkpoint> [options]
+        \\Example: run model.bin -n 256 -i \"Once upon a time\"
+        \\Options:\n");
+        \\  -t <float>  temperature in [0,inf], default 1.0
+        \\  -p <float>  p value in top-p (nucleus) sampling in [0,1] default 0.9
+        \\  -s <int>    random seed, default time(NULL)
+        \\  -n <int>    number of steps to run for, default 256. 0 = max_seq_len
+        \\  -i <string> input prompt
+        \\  -z <string> optional path to custom tokenizer
+        \\  -m <string> mode: generate|chat, default: generate
+        \\  -y <string> (optional) system prompt in chat mode
+    ;
+    try stderr.print(usage, .{});
+}
+
 pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+    var general_purpose_allocator = std.heap.GeneralPurposeAllocator(.{}){};
+    const gpa = general_purpose_allocator.allocator();
 
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
+    var checkpoint_path: ?[]const u8 = null;
+    // Parse command line arguments
+    const args = try std.process.argsAlloc(gpa);
+    defer std.process.argsFree(gpa, args);
 
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
+    if (args.len < 2) {
+        try errorUsage();
+        std.os.exit(1);
+    }
+    checkpoint_path = args[1];
 
-    try bw.flush(); // don't forget to flush!
+    // TODO: more command line argument validation
+
+    const transformer = try transformerInit(checkpoint_path.?);
+    _ = transformer;
 }
