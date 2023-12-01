27fd9efcf16f65b63aa35bd5131b074f4161e78e
Apoorva Joshi <apoorvaj@apoorvaj.io>
2023-12-01 15:58:34 +0100

    Slice memory mapped weights

--- src/main.zig
+++ src/main.zig
@@ -10,10 +10,46 @@ const Config = struct {
     seq_len: i32, // max sequence length
 };
 
+const Weights = struct {
+    // Token embedding table
+    token_embedding_table: []align(1) const f32, // (vocab_size, dim)
+    // Weights for rmsnorms
+    rms_att_weight: []align(1) const f32, // (layer, dim) rmsnorm weights
+    rms_ffn_weight: []align(1) const f32, // (layer, dim)
+    // Weights for matmuls. note dim == n_heads * head_size
+    wq: []align(1) const f32, // (layer, dim, n_heads * head_size)
+    wk: []align(1) const f32, // (layer, dim, n_kv_heads * head_size)
+    wv: []align(1) const f32, // (layer, dim, n_kv_heads * head_size)
+    wo: []align(1) const f32, // (layer, n_heads * head_size, dim)
+    // Weights for ffn
+    w1: []align(1) const f32, // (layer, hidden_dim, dim)
+    w2: []align(1) const f32, // (layer, dim, hidden_dim)
+    w3: []align(1) const f32, // (layer, hidden_dim, dim)
+    // Final rmsnorm
+    rms_final_weight: []align(1) const f32, // (dim,)
+    // (optional) classifier weights for the logits, on the last layer
+    wcls: []align(1) const f32,
+};
+
 const Transformer = struct {
     config: Config,
     fd: std.os.fd_t,
+    mapped_buffer: []align(std.mem.page_size) const u8,
+    weights: Weights,
 };
+
+/// This function returns a slice of f32s from the source u8 slice. It also increments the start
+/// address.
+fn slice_and_increment(
+    src: []align(std.mem.page_size) const u8,
+    start: *usize,
+    num_f32s: usize,
+) []align(1) const f32 {
+    const end: usize = start.* + @sizeOf(f32) * num_f32s;
+    const out_slice = std.mem.bytesAsSlice(f32, src[(start.*)..end]);
+    start.* = end;
+    return out_slice;
+}
 
 fn transformerInit(checkpoint_path: []const u8) !Transformer {
     var file_size: u64 = 0;
@@ -34,23 +70,57 @@ fn transformerInit(checkpoint_path: []const u8) !Transformer {
     }
     // Memory map the Transformer weights into the data pointer
     var fd: std.os.fd_t = undefined;
+    var mapped_buffer: []align(std.mem.page_size) const u8 = undefined;
     {
         fd = try std.os.open(checkpoint_path, 0, 0);
         // TODO: This relies on Linux-specific mmap flags. Need to make this portable.
-        const data = try std.os.mmap(null, file_size, std.os.linux.PROT.READ, std.os.linux.MAP.PRIVATE, fd, 0);
-        _ = data;
+        mapped_buffer = try std.os.mmap(null, file_size, std.os.linux.PROT.READ, std.os.linux.MAP.PRIVATE, fd, 0);
+    }
+    // Obtain slices into the mapped buffer for each of the weights
+    var weights: Weights = undefined;
+    {
+        std.debug.assert(config.dim > 0 and config.n_heads > 0 and config.n_layers > 0 and config.vocab_size > 0 and config.n_heads > 0);
+        const n_layers: usize = @intCast(config.n_layers);
+        const dim: usize = @intCast(config.dim);
+        const hidden_dim: usize = @intCast(config.hidden_dim);
+        const vocab_size: usize = @intCast(config.vocab_size);
+        const n_heads: usize = @intCast(config.n_heads);
+        const n_kv_heads: usize = @intCast(config.n_kv_heads);
+        const seq_len: usize = @intCast(config.seq_len);
+        const head_size: usize = @divTrunc(dim, n_heads);
+
+        var start: usize = @sizeOf(Config);
+        weights.token_embedding_table = slice_and_increment(mapped_buffer, &start, vocab_size * dim);
+        weights.rms_att_weight = slice_and_increment(mapped_buffer, &start, n_layers * dim);
+        weights.wq = slice_and_increment(mapped_buffer, &start, n_layers * dim * (n_heads * head_size));
+        weights.wk = slice_and_increment(mapped_buffer, &start, n_layers * dim * (n_kv_heads * head_size));
+        weights.wv = slice_and_increment(mapped_buffer, &start, n_layers * dim * (n_kv_heads * head_size));
+        weights.wo = slice_and_increment(mapped_buffer, &start, n_layers * dim * (n_heads * head_size));
+        weights.rms_ffn_weight = slice_and_increment(mapped_buffer, &start, n_layers * dim);
+        weights.w1 = slice_and_increment(mapped_buffer, &start, n_layers * dim * hidden_dim);
+        weights.w2 = slice_and_increment(mapped_buffer, &start, n_layers * dim * hidden_dim);
+        weights.w3 = slice_and_increment(mapped_buffer, &start, n_layers * dim * hidden_dim);
+        weights.rms_final_weight = slice_and_increment(mapped_buffer, &start, dim);
+        if (shared_weights) {
+            weights.wcls = weights.token_embedding_table;
+        } else {
+            start += seq_len * head_size / 2; // skip what used to be freq_cis_real (for RoPE)
+            start += seq_len * head_size / 2; // skip what used to be freq_cis_imag (for RoPE)
+            weights.wcls = slice_and_increment(mapped_buffer, &start, vocab_size * dim);
+        }
     }
 
     return .{
         .config = config,
         .fd = fd,
+        .mapped_buffer = mapped_buffer,
+        .weights = weights,
     };
 }
 
 fn transformerFree(transformer: Transformer) void {
-    _ = std.os.close(transformer.fd);
-    // TODO: Unamp the memory
-    // std.os.munmap(data);
+    std.os.close(transformer.fd);
+    std.os.munmap(transformer.mapped_buffer);
 }
 
 fn errorUsage() !void {
@@ -89,5 +159,5 @@ pub fn main() !void {
     // TODO: more command line argument validation
 
     const transformer = try transformerInit(checkpoint_path.?);
-    transformerFree(transformer);
+    defer transformerFree(transformer);
 }
