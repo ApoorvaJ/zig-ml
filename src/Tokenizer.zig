08cc86962678238470b9e6c6bbed8d2e2345c751
Apoorva Joshi <apoorvaj@apoorvaj.io>
2023-12-02 18:56:51 +0100

    More tokenizer initialization

--- src/Tokenizer.zig
+++ src/Tokenizer.zig
@@ -8,18 +8,26 @@ const TokenIndex = struct {
 vocab: [][:0]const u8,
 vocab_scores: []f32,
 sorted_vocab: []TokenIndex,
+// TODO: Ensure this is necessary. Since the (i * 2)th element contains the character i, we might
+// be able to get away with computing this value on the fly.
+byte_pieces: [512]u8, // stores all single-byte strings
 // max_token_length: u32,
-// byte_pieces: [512]u8, // stores all single-byte strings
 
 pub fn init(tokenizer_path: []const u8, allocator: std.mem.Allocator, vocab_size: usize) !@This() {
     _ = tokenizer_path;
     var vocab: [][:0]u8 = try allocator.alloc([:0]u8, vocab_size);
     var vocab_scores: []f32 = try allocator.alloc(f32, vocab_size);
     var sorted_vocab: []TokenIndex = try allocator.alloc(TokenIndex, vocab_size);
+    var byte_pieces: [512]u8 = undefined;
+    for (0..256) |i| {
+        byte_pieces[i * 2] = @intCast(i);
+        byte_pieces[i * 2 + 1] = 0;
+    }
     return .{
         .vocab = vocab,
         .vocab_scores = vocab_scores,
         .sorted_vocab = sorted_vocab,
+        .byte_pieces = byte_pieces,
     };
 }
 
