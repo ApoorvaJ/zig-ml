76d928875dcef0ed4db8f3803a4742892788c677
Apoorva Joshi <apoorvaj@apoorvaj.io>
2023-12-04 17:42:36 +0100

    Implement token merging during encoding

--- src/Tokenizer.zig
+++ src/Tokenizer.zig
@@ -99,11 +99,9 @@ pub fn encode(
     out_tokens: []u32,
     allocator: std.mem.Allocator,
 ) !usize {
-    _ = allocator;
     // bos != 0 means prepend the BOS token (=1), eos != 0 means append the EOS token (=2)
     const prepend_bos_token: bool = true;
     const append_eos_token: bool = false;
-    _ = append_eos_token;
 
     var n_tokens: usize = 0;
 
@@ -125,13 +123,6 @@ pub fn encode(
         n_tokens += 1;
     }
 
-    // Create a temporary buffer that will store merge candidates of always two consecutive tokens
-    // *2 for concat, +1 for null terminator +2 for UTF8 (in case max_token_length is 1)
-    const max_len: usize = @intCast(self.max_token_length);
-    _ = max_len;
-    // var str_buffer = try allocator.alloc(u8, max_len * 2 + 1 + 2);
-    // defer allocator.free(str_buffer);
-
     // Convert each UTF-8 codepoint into a token
     var i: usize = 0;
     while (i < text.len) {
@@ -146,17 +137,15 @@ pub fn encode(
             // This should never happen, because we're always parsing whole codepoints.
             return error.InputIsInvalidUTF8;
         }
-
+        // Get a slice into the text that contains the codepoint
         const codept: []const u8 = text[i .. i + codept_len];
-        const opt_idx: ?usize = std.sort.binarySearch(TokenIndex, codept, self.sorted_vocab, {}, compareToken);
-
-        if (opt_idx != null) {
+        if (std.sort.binarySearch(TokenIndex, codept, self.sorted_vocab, {}, compareToken)) |idx| {
             // We found this codepoint in vocab. Add it as a token.
-            out_tokens[n_tokens] = self.sorted_vocab[opt_idx.?].id;
+            out_tokens[n_tokens] = self.sorted_vocab[idx].id;
             n_tokens += 1;
         } else {
             // We didn't find this codepoint in vocab.
-            // Use byte_fallback encoding: just encode each byte as a token.
+            // Use byte fallback encoding: just encode each byte as a token.
             // +3 is here because the first 3 vocab elements are <unk>, <s>, </s>
             // so the individual bytes only start at index 3
             for (codept) |byte| {
@@ -166,6 +155,55 @@ pub fn encode(
         }
 
         i += codept_len;
+    }
+
+    // Create a temporary buffer that will store merge candidates of always two consecutive tokens
+    // *2 for concat, +1 for null terminator +2 for UTF8 (in case max_token_length is 1)
+    const max_len: usize = @intCast(self.max_token_length);
+    var str_buffer = try allocator.alloc(u8, max_len * 2 + 1 + 2);
+    defer allocator.free(str_buffer);
+
+    while (true) {
+        var best_score: f32 = -1e10;
+        var opt_best_id: ?u32 = null;
+        var opt_best_idx: ?usize = null;
+
+        for (0..n_tokens - 1) |j| {
+            const t0: []const u8 = self.vocab[out_tokens[j]];
+            const t1: []const u8 = self.vocab[out_tokens[j + 1]];
+            @memcpy(str_buffer[0..t0.len], t0);
+            @memcpy(str_buffer[t0.len .. t0.len + t1.len], t1);
+            // At this point, the str_buffer contains the concatenation of the two tokens, plus
+            // some gargabe at the end. We take a slice of it that contains only the two tokens.
+            const combined_token: []const u8 = str_buffer[0 .. t0.len + t1.len];
+
+            const opt_id = std.sort.binarySearch(TokenIndex, combined_token, self.sorted_vocab, {}, compareToken);
+            if (opt_id) |id| {
+                if (best_score < self.vocab_scores[id]) {
+                    best_score = self.vocab_scores[id];
+                    opt_best_id = @intCast(id);
+                    opt_best_idx = j;
+                }
+            }
+        }
+
+        if (opt_best_idx == null) {
+            // ^ We couldn't find any more pairs to merge, so we're done.
+            break;
+        }
+
+        // Merge the consecutive pair (best_idx, best_idx+1) into new token best_id
+        out_tokens[opt_best_idx.?] = opt_best_id.?;
+        // Left shift the rest of the array. Since the source and the destination overlap, we have
+        // to use this function instead of @memcpy.
+        std.mem.copyForwards(u32, out_tokens[opt_best_idx.? + 1 .. n_tokens - 1], out_tokens[opt_best_idx.? + 2 .. n_tokens]);
+        n_tokens -= 1;
+    }
+
+    // add optional EOS (=2) token, if desired
+    if (append_eos_token) {
+        out_tokens[n_tokens] = 2;
+        n_tokens += 1;
     }
 
     return n_tokens;
